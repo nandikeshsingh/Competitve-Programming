6610ac2d61f1f9a220d0d87aa86f44470d2835b2
nandikeshsingh <nandikeshsingh@gmail.com>
2022-09-18 02:13:24 +0530

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -1,5 +1,5 @@
-vector<int>parent(n+1,0);
-vector<int>sz(n+1,0);
+vector<int>parent;//initialize this after taking the input
+vector<int>sz;//initialize this after taking the input
 void make_set(int v) {
     parent[v] = v;
     sz[v] = 1;
@@ -9,7 +9,7 @@ int find_set(int v) {
         return v;
     return parent[v] = find_set(parent[v]);
 }
-void union_sets(int a, int b) {
+void merge(int a, int b) {
     a = find_set(a);
     b = find_set(b);
     if (a != b) {
