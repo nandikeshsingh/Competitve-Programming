290a657c85f88893d4c7e3b23152cf4c87461cbb
nandikeshsingh <nandikeshsingh@gmail.com>
2022-09-20 16:29:51 +0530

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -2,7 +2,7 @@ vector<int>parent;//initialize this after taking the input
 vector<int>sz;//initialize this after taking the input
 void make_set(int v) {
     parent[v] = v;
-    sz[v] = 1;
+    size[v] = 1;
 }
 int find_set(int v) {
     if (v == parent[v])
@@ -13,9 +13,9 @@ void merge(int a, int b) {
     a = find_set(a);
     b = find_set(b);
     if (a != b) {
-        if (sz[a] < sz[b])
+        if (size[a] < size[b])
             swap(a, b);
         parent[b] = a;
-        sz[a] += sz[b];
+        size[a] += size[b];
     }
 }
