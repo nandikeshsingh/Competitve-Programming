6b3577509ccae2ade81ef78b7eebe128c3b8628d
nandikeshsingh <nandikeshsingh@gmail.com>
2023-12-09 02:41:15 +0530

    Create Trie.cpp

---
+++ Trie.cpp
@@ -0,0 +1,37 @@
+//M is the max length of the string to be inserted.
+//A is the total number of distinct charaters.
+struct Trie {
+    int G = 0;
+    int t[M][A] = {};
+    int f[M] = {};
+ 
+    void add(const string& s) {
+        int x = 0;
+        for (int i = 0; i < s.size(); i++) {
+            int c = s[i] - 'a';
+ 
+            if (!t[x][c]) {
+                t[x][c] = ++G;
+            }
+ 
+            x = t[x][c];
+            f[x] += 1;
+        }
+    }
+ 
+    long long get(const string& s) {
+        int x = 0;
+        long long res = 0;
+        for (int i = 0; i < s.size(); i++) {
+            int c = s[i] - 'a';
+ 
+            if (!t[x][c]) {
+                break;
+            }
+ 
+            x = t[x][c];
+            res += f[x];
+        }
+        return res;
+    }
+};
