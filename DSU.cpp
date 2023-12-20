21ccda69934762f4b9be75c4c91bcef7a4f4a480
nandikeshsingh <nandikeshsingh@gmail.com>
2023-12-20 17:54:45 +0530

    Update DSU.cpp

--- DSU.cpp
+++ DSU.cpp
@@ -27,3 +27,50 @@ void init(int n)//n is the size of the input(graph)
     sz.resize(n+1);
     for(int i=1;i<=n;i++)make_set(i);
 }
+
+//Struct for DSU
+struct DSU
+{
+	int connected;
+	vector<int> par, sz;
+
+	void init(int n) 
+	{
+		par = sz = vector<int> (n + 1, 0);
+		for(int i = 1; i <= n; i++)
+			par[i] = i, sz[i] = 1;
+		connected = n;
+	}
+
+	int getPar(int u)
+	{
+		while(u != par[u])
+		{
+			par[u] = par[par[u]];
+			u = par[u];
+		}
+		return u;
+	}
+
+	int getSize(int u)
+	{
+		return sz[getPar(u)];
+	}
+
+	void merge(int u, int v)
+	{
+		int par1 = getPar(u), par2 = getPar(v);
+
+		if(par1 == par2)
+			return;
+
+		connected--;
+
+		if(sz[par1] > sz[par2])
+			swap(par1, par2);
+
+		sz[par2] += sz[par1];
+		sz[par1] = 0;
+		par[par1] = par[par2];
+	}
+};
