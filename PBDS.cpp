d264ba578605f5133c2d5aceb57d8cf4d7044696
nandikeshsingh <nandikeshsingh@gmail.com>
2024-03-19 17:18:45 +0530

    Update PBDS.cpp

--- PBDS.cpp
+++ PBDS.cpp
@@ -6,6 +6,7 @@ using namespace __gnu_pbds;
 #define int long long
 #define speed  ios_base::sync_with_stdio(false); cin.tie(NULL);
 #define all(v) v.begin(),v.end()
+#define sz(v) (int)v.size()
 #define ordered_set tree<pair<int,int>, null_type,less<pair<int,int>>, rb_tree_tag,tree_order_statistics_node_update>
 signed main()
 {
